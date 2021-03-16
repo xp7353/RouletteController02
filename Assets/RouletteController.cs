557cc45d0760e6cf314e3185e06c3d1303b11327
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-03-16 12:11:09 +0900

    Initial commit

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { this.rotSpeed = 30.0f; }
+        if (Input.GetMouseButtonDown(2)) { this.rotSpeed = 0.0f; }
+        if (Input.GetMouseButtonDown(1)) { this.rotSpeed = -30.0f; }
+
+        this.rotSpeed *= 0.987f;
+        transform.Rotate(0, 0, this.rotSpeed);
+    }
+}
